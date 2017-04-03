8344348fac963d3b325ced20f91e13c60d064a3e
dmitriykirilyuk <dimasishere@gmail.com>
2017-04-03 23:40:31 +0300

    закончил график

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -13,7 +13,7 @@ window.renderStatistics = function (ctx, names, times) {
 
   var max = -1;
   var maxIndex = -1;
-  for(var i = 0; i < times.length; i++) {
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
@@ -21,10 +21,10 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  var histogramHeight = 150;
+  var histogramHeight = 120;
   var step = histogramHeight / (max - 0);
 
-  ctx.fillText ('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 80);
+  ctx.fillText('Худшее время: ' + max.toFixed(2) + 'мс у игрока ' + names[maxIndex], 120, 80);
 
   var barWidth = 40;
   var indent = 50;
@@ -32,16 +32,22 @@ window.renderStatistics = function (ctx, names, times) {
   var initialY = 100;
 
   ctx.textBaseline = 'top';
-  for (var i = 0; i < times.length; i++) {
-    ctx.fillRect(initialX, initialY + indent * i, times[i] * step, barWidth);
-    ctx.fillText(names[i], initialX + histogramHeight, initialY + indent * i);
-    if (name === 'Вы!') {
+  for (i = 0; i < times.length; i++) {
+    var positionX = initialX + i * (barWidth + indent);
+    var positionY = initialY + histogramHeight + 30;
+    var timeTracker = times[i].toFixed(0);
+    var name = names[i];
+    ctx.fillStyle = '#000';
+    ctx.fillText(timeTracker, positionX, 100);
+    ctx.fillText(name, positionX, positionY);
+    if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = ['rgba(21, 96, 189, ', (Math.random()).toFixed(1), ')'].join('');
     }
+    ctx.fillRect(positionX, positionY, barWidth, times[i] * -step);
   }
 };
 
 var canvas = document.querySelector('canvas');
-window.renderStatistics(canvas.getContext('2d'), ['Ира', 'Саша', 'Макс', 'Оля', 'Вы!'], [5.20, 12.59, 23.66, 7.12, 15.34]);
+window.renderStatistics(canvas.getContext('2d'), ['Ира', 'Саша', 'Макс', 'Оля', 'Вы'], [5.20, 12.59, 23.66, 7.12, 15.34]);
